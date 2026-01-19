effef1cd370bbc96c120e49731e03682d54fe6b7
Zhe Wang <zachery@login-el8.khoury.northeastern.edu>
2026-01-19 17:29:44 -0500

    renew file myprocess.c

--- my_process/myprocess.c
+++ my_process/myprocess.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+int gval = 100;
 int main()
 {
-	while(1)
+	fprintf(stdout, "Parent process start, pid = %d\n", getpid());
+	pid_t id = fork();
+	
+	if(id < 0)
 	{
-		sleep(2);
-		fprintf(stdout, "I'm a process! my pid is %d! my parent pid is %d!\n", getpid(), getppid());
+		perror("fork fail\n");
+		return 1;
 	}
+	else if(id == 0)
+	{
+		// child
+		while(1)
+		{
+			sleep(3);
+			fprintf(stdout, "I'm a child process, my pid is %d, my parent pid is %d.\n", getpid(), getppid());
+			fprintf(stdout, "change gval %d->%d\n", gval, gval + 1);
+			++gval;
+		}
+	}
+	else
+	{
+		// parent
+		while(1)
+		{
+			sleep(3);
+			fprintf(stdout, "I'm a parent process, my pid is %d, my parent pid is %d. gval = %d\n", getpid(), getppid(), gval);
+		}
+	}
+	
+	//chdir("/home/zachery");
+//	while(1)
+//	{
+//		sleep(2);
+//		fprintf(stdout, "I'm a process! my pid is %d! my parent pid is %d!\n", getpid(), getppid());
+//	}
 }
